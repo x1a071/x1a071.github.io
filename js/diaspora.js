ba2044827a5b1a8a7cf517281a168c47688a2cc6
x1a071 <1395530878@qq.com>
2021-05-05 23:00:39 +0800

    Site updated: 2021-05-05 23:00:37

--- js/diaspora.js
+++ js/diaspora.js
@@ -147,10 +147,9 @@ var Diaspora = {
             p[0].src = mp3.data('url')
         }
 
-        setTimeout( function(){}, 1 * 1000 )
-            if (p.eq(0).data("autoplay") == true) {
-                p[0].play();
-            }
+        if (p.eq(0).data("autoplay") == true) {
+            p[0].play();
+        }
 
         p.on({
             'timeupdate': function() {
